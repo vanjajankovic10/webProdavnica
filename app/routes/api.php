2c30f6fd4acdedf28cd52a4afad58048c1151796
jovanajeremic <jovana00jeremic@gmail.com>
2023-08-29 23:49:29 +0200

    kreirane api rute

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('products',[ProductController::class,'index']);
+Route::get('products/{id}',[ProductController::class,'show']);
+
+
+
+Route::get('categories',[CategoryController::class,'index']);
+
+
+
+ 
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+Route::post('logout',[AuthController::class,'logout']);
+
+Route::post('orders',[OrderController::class,'store']);
+Route::post('orderItems',[OrderItemController::class,'store']);
+
+Route::post('products',[ProductController::class,'store']);
+Route::put('products/{id}',[ProductController::class,'update']);
+Route::delete('products/{id}',[ProductController::class,'destroy']);
+Route::get('orders',[OrderController::class,'index']);
+Route::get('orderItems',[OrderItemController::class,'index']);
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {  
+     
+
+
 });
+
+
+ 
